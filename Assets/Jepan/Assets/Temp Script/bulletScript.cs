7d7b1485117c060d79481f167fbbc44e3d39ecc1
catashX <agustinuszefanya@gmail.com>
2023-04-12 11:49:50 +0700

    tambahan diki

--- Assets/Jepan/Assets/Temp Script/bulletScript.cs
+++ Assets/Jepan/Assets/Temp Script/bulletScript.cs
@@ -6,14 +6,43 @@ public class bulletScript : MonoBehaviour
 {
     // Start is called before the first frame update
     public float bulletLifeTime = 0.75f;
+    public float deadTime = 0.5f;
+    Animator anim;
+    Rigidbody2D rb;
+    ParticleSystem ps;
+    float lifetime = 0;
     void Start()
     {
-        Destroy(gameObject,bulletLifeTime);
+        rb = GetComponent<Rigidbody2D>();
+        ps = GetComponent<ParticleSystem>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        lifetime += Time.deltaTime;
+        if(lifetime > bulletLifeTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void onDamaging()
+    {
+        lifetime = 0;
+        rb.velocity = Vector3.zero;
+        rb.bodyType = RigidbodyType2D.Static;
+        Destroy(gameObject, deadTime);
+        anim.SetTrigger("Destroy");
+        ps.Play();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            onDamaging();
+        }
     }
 }
